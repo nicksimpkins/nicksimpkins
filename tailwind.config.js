023f3c7ff46d4d2b603b75d2245913071310cd30
Nick Simpkins <nsimpkins@hawk.iit.edu>
2023-04-18 18:00:35 -0500

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./scripts/*.{js}", "./style/*.{css}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
